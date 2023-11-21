7650a16cbdddcc41e979beabf9ab10ccb4602f7d
R3moulade <maddie.dyrby.hansen@outlook.dk>
2023-11-21 14:20:06 +0100

    finished layout preview trip component

--- src/components/PreviewTrip.jsx
+++ src/components/PreviewTrip.jsx
@@ -3,18 +3,29 @@ import "../css/PreviewTrip.css";
 
 const PreviewTrip = () => {
     return <div className="preview-wrapper">
-        <div>
+        <div className="preview-image-container">
             <img src={placeholder} alt="Preview Image" />
-
+            <div className="preview-trip-bubble preview-location">
+                <p>Lolland - Bornholm</p>
+            </div>
         </div>
         <div className="description-wrapper">
             <div>
                 <h3>Den grimme perle</h3>
                 <span>15 nov - 1 dec </span>
                 <span>· 0 stops</span>
+                <div className="hashtag-container">
+                    <div className="preview-trip-bubble hashtag">
+                        <p>smoke-free</p>
+                    </div>
+                    <div className="preview-trip-bubble hashtag">
+                        <p>ø-hop</p>
+                    </div>
+                </div>
+
             </div>
             <div>
-                <div className="preview-trip-price">
+                <div className="preview-trip-bubble">
                     <p>$40</p>
                     <span>per day</span>
                 </div>

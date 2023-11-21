d1b42dc32a921a5ea6520fa3703d8abe15b7edbd
R3moulade <maddie.dyrby.hansen@outlook.dk>
2023-11-21 13:41:34 +0100

    image, price, title, date, stops

--- src/components/PreviewTrip.jsx
+++ src/components/PreviewTrip.jsx
@@ -1,4 +1,5 @@
 import placeholder from "../assets/images/boat.jpg";
+import "../css/PreviewTrip.css";
 
 const PreviewTrip = () => {
     return <div className="preview-wrapper">
@@ -6,8 +7,18 @@ const PreviewTrip = () => {
             <img src={placeholder} alt="Preview Image" />
 
         </div>
-        <div>
-
+        <div className="description-wrapper">
+            <div>
+                <h3>Den grimme perle</h3>
+                <span>15 nov - 1 dec </span>
+                <span>· 0 stops</span>
+            </div>
+            <div>
+                <div className="preview-trip-price">
+                    <p>$40</p>
+                    <span>per day</span>
+                </div>
+            </div>
         </div>
     </div>;
   };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,10 @@ import PreviewTrip from "../components/PreviewTrip";
 
 const Home = () => {
     return (
-        <div>
+        <div style={{padding: '1em', marginBottom: '80px'}}>
             <h1>Recommend trips</h1>
             <PreviewTrip />
+
         </div>
     );
 };

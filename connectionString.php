f174cfa0a56c6f88051feb54a91099cc1e432112
Stefan Langkilde <eaa23stla@students.eaaa.dk>
2023-11-22 10:17:44 +0100

    removed mistake

--- connectionString.php
+++ connectionString.php
@@ -1,9 +1,5 @@
 <?php 
-    $server = "mysql36.unoeuro.com";
-    $user = "stefanoskl_dk";
-    $password = "bDmcpFwh9azkyxGfBdn4";
-    $database = "stefanoskl_dk_db";
-
+    
     $mySQL = new mysqli($server, $user, $password, $database);
 
     if(!$mySQL) {

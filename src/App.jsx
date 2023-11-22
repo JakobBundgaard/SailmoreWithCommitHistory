ff7d593d86fe0c07f749b676e4741e14bbe310b7
JakobBundgaard <jakobbundgaard@hotmail.com>
2023-11-22 09:04:16 +0100

    Component import removed before merge
    CrewSignup component removed from App.jsx before merging into main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import CrewSignup from "./components/CrewSignup";
+
 
 function App() {
    return (
       <div>
          <h1>React App</h1>
-         <CrewSignup />
       </div>
    );
 }

16dc03f0d68083bfc6259b6809e200796edf2884
Stefan Langkilde <eaa23stla@students.eaaa.dk>
2023-11-22 09:39:36 +0100

    remove app.css style, added hande input change also added a captain picture

--- src/components/CaptainSignup.jsx
+++ src/components/CaptainSignup.jsx
@@ -1,8 +1,46 @@
 import "../css/captainSignup.css";
 import captainImage from "../assets/images/captain.jpg";
+import { useState } from "react";
 
 function CaptainSignup() {
-    return (
+   const [name, setName] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   const [nationality, setNationality] = useState("");
+   const [bio, setBio] = useState("");
+   const [gender, setGender] = useState("");
+   const [age, setAge] = useState("");
+
+   const handleInputChange = event => {
+      switch (event.target.name) {
+         case "name":
+            setName(event.target.value);
+            break;
+         case "email":
+            setEmail(event.target.value);
+            break;
+         case "password":
+            setPassword(event.target.value);
+            break;
+         case "nationality":
+            setNationality(event.target.value);
+            break;
+         case "bio":
+            setBio(event.target.value);
+            break;
+         case "gender":
+            setGender(event.target.value);
+            break;
+         case "age":
+            setAge(event.target.value);
+            break;
+         default:
+            console.log("Invalid input");
+            break;
+      }
+   };
+
+   return (
       <div className="capLoginPage">
          <h1>Captain Signup</h1>
          <img src={captainImage} alt="Captain Signup" />
@@ -11,29 +49,29 @@ function CaptainSignup() {
                <label className="flexItem" htmlFor="name">
                   Name
                </label>
-               <input className="flexItem" type="text" name="name" />
+               <input className="flexItem" type="text" name="name" value={name} onChange={handleInputChange} />
                <label className="flexItem" htmlFor="email">
                   Email
                </label>
-               <input className="flexItem" type="text" name="email" />
+               <input className="flexItem" type="text" name="email" value={email} onChange={handleInputChange} />
                <label className="flexItem" htmlFor="password">
                   Password
                </label>
-               <input className="flexItem" type="password" name="password" />
+               <input className="flexItem" type="password" name="password" value={password} onChange={handleInputChange} />
                <label className="flexItem" htmlFor="nationality">
                   Nationality
                </label>
-               <input className="flexItem" type="text" name="nationality" />
+               <input className="flexItem" type="text" name="nationality" value={nationality} onChange={handleInputChange} />
                <label className="flexItem" htmlFor="bio">
                   Bio
                </label>
-               <textarea className="flexItem" type="text" name="bio" />
+               <textarea className="flexItem" type="text" name="bio" value={bio} onChange={handleInputChange} />
                <div className="flexRow">
                   <div className="flexCol">
                      <label className="flexItem slim" htmlFor="gender">
                         Gender
                      </label>
-                     <select className="flexItem" id="gender" name="gender">
+                     <select className="flexItem" id="gender" name="gender" value={gender} onChange={handleInputChange}>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
                         <option value="Other">Other</option>
@@ -43,7 +81,7 @@ function CaptainSignup() {
                      <label className="flexItem slim" htmlFor="age">
                         Age
                      </label>
-                     <input className="flexItem slim" type="number" name="age" />
+                     <input className="flexItem slim" type="number" name="age" value={age} onChange={handleInputChange} />
                   </div>
                </div>
                <div className="flexRow">

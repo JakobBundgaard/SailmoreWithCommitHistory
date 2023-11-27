259aaece9684d8835a0062d0e0da24fbaea2f308
mettemarie <mettemariebech@hotmail.com>
2023-11-27 13:17:26 +0100

    Temporary finished BoatDetails page

--- src/pages/BoatDetails.jsx
+++ src/pages/BoatDetails.jsx
@@ -1,18 +1,21 @@
 import { Outlet, NavLink } from "react-router-dom";
 import placeholder from "../assets/images/boat2.jpg";
-import pfp from "../assets/images/boat2.jpg";
+import pfboat from "../assets/images/boat2.jpg";
+import pfp from "../assets/images/captain.jpg";
 import certified from "../assets/images/certified-badge.svg";
+import "../css/BoatDetails.css";
 import "../css/PreviewTrip.css";
 import "../css/SkipperProfile.css";
+import BackArrow from "../components/BackArrow";
 
 const BoatDetails = () => {
     return (
         <div >
             <div className="top-panel">
-                <p>backbutton component</p>
+                <BackArrow />
             </div>
             <div className='profile-images'>
-                <img src={pfp} alt="Profile Image" />
+                <img src={pfboat} alt="Profile Image" />
             </div>
             <div className="page-wrapper">
                 <div className='profile-top'>
@@ -31,8 +34,18 @@ const BoatDetails = () => {
                     </div> */}
                 </div>
             <hr />
-            <h2>About Me</h2>
-            <p>Ahoy! Im Skræk, a sea-loving adventurer with a passion for boat vacations. Whether its a sailboat or a yacht, Im all about making waves and unforgettable memories. Join me as I navigate life with a compass in hand and a splash of personality. Ready for some nautical fun? #SailorLife</p>
+            <h2 className="about-me-boat">About Me</h2>
+            <p className="boat-description">🚤 Ahoy there! I'm Den grimme perle, a vintage vessel with stories etched in every plank. Seeking a mate to appreciate sunsets, sea breezes, and the timeless charm of well-aged wood. If you're ready for a journey with a touch of history and a lot of heart, let's set sail together. ⚓️🌅 #VintageVoyager #SeaStories</p>
+            <hr />
+            <NavLink to="/skipper/:id">
+                    <article className="pfp-wrapper">
+                        <img src={pfp} alt="Profile picture of {username}" className="pfp" />
+                        <div>
+                            <img src={certified} alt="Certified Badge" />
+                            <h4>Skipper Skræk</h4>
+                        </div>
+                    </article>
+                </NavLink>
             </div>
         </div>
         )

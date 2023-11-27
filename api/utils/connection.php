14682de265eae68fc5df16934a00e31e6c4cb98c
JakobBundgaard <jakobbundgaard@hotmail.com>
2023-11-27 11:12:21 +0100

    Proxy server done
    Test getting data from db

--- api/utils/connection.php
+++ api/utils/connection.php
@@ -2,9 +2,9 @@
 
 include_once "dbConfig.php";
     
-    $mySQL = new mysqli($server, $user, $password, $database);
+    $conn = new mysqli($server, $user, $password, $database);
 
-    if(!$mySQL) {
+    if(!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }else {
     echo "Connected successfully";

f8ab895b6b4b01ec0eb09e00bd137e67387df291
JakobBundgaard <jakobbundgaard@hotmail.com>
2023-12-05 11:19:21 +0100

    Searchbar getting the correct data in response

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,6 +1,5 @@
 import "../css/SearchBar.css";
 
-
 import { useState } from 'react';
 
 const SearchBar = () => {
@@ -10,48 +9,77 @@ const SearchBar = () => {
   const [maxPrice, setMaxPrice] = useState('');
 
   // Function to handle search based on criteria
-  const handleSearch = () => {
-    // Customize this function to perform the actual search
-    console.log('Search Criteria:', { date, location, maxPrice });
+  const handleSearch = async () => {
+    try {
+      // Directly use the relative path of the endpoint
+      const endpoint = 'api/search/tripSearch.php';
+
+      // Build URL parameters based on user input
+      const params = new URLSearchParams();
+      if (date) params.append('date', date);
+      if (location) params.append('location', location);
+      if (maxPrice) params.append('maxPrice', maxPrice);
+
+      // Use the relative path and append parameters
+      const url = `${endpoint}?${params.toString()}`;
+
+      // Fetch data from the API
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      // Parse the JSON response
+      const data = await response.json();
+      console.log('Search Results:', data);
+
+      // Handle the results (update state, display, etc.)
+      // For example, you might set the search results in state for rendering
+      // setResults(data);
+    } catch (error) {
+      console.error('Error during search:', error);
+    }
   };
 
   return (
     <div className="search-bar-container">
-    <div className="search-input-container">
-      <label className="search-label">
-        Date:
-        <input
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-          className="search-input date-input"
-        />
-      </label>
-      <label className="search-label">
-        Location:
-        <input
-          type="text"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-          className="search-input"
-        />
-      </label>
-      <label className="search-label">
-        Max Price:
-        <input
-          type="number"
-          value={maxPrice}
-          onChange={(e) => setMaxPrice(e.target.value)}
-          className="search-input"
-        />
-      </label>
+      <div className="search-input-container">
+        <label className="search-label">
+          Date:
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            className="search-input date-input"
+          />
+        </label>
+        <label className="search-label">
+          Location:
+          <input
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            className="search-input"
+          />
+        </label>
+        <label className="search-label">
+          Max Price:
+          <input
+            type="number"
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(e.target.value)}
+            className="search-input"
+          />
+        </label>
+      </div>
+      <button onClick={handleSearch} className="search-button">
+        Search
+      </button>
     </div>
-    <button onClick={handleSearch} className="search-button">
-      Search
-    </button>
-  </div>
   );
 };
 
 export default SearchBar;
+
  

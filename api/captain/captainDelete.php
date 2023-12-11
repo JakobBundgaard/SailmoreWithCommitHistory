078d1ad0df6bab824c7a315afc03e0ea9e241416
Stefan Langkilde <eaa23stla@students.eaaa.dk>
2023-12-11 09:25:50 +0100

    Add captainDelete.php for deleting a captain

---
+++ api/captain/captainDelete.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once '../utils/connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    if (!isset($data['captainId'])) {
+        $response = array("error" => "Invalid request, captain not deleted");
+        echo json_encode($response);
+        exit();
+    }
+
+    $captainId = $data['captainId'];
+
+    $query = "DELETE FROM captain WHERE captainId = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('i', $captainId);
+
+    try {
+        $stmt->execute();
+        $response = array("success" => "Captain deleted successfully");
+    } catch (mysqli_sql_exception $e) {
+        $response = array("error" => $e->getMessage());
+        echo json_encode($response);
+        exit();
+    }
+    
+    echo json_encode($response);
+} else {
+    $response = array("error" => "Invalid request method, captain not deleted");
+    echo json_encode($response);
+}
+
+?>

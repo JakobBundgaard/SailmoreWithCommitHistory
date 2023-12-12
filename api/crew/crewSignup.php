d2f936e5cf1b3b84841a2713d0c53919f9fd264c
R3moulade <maddie.dyrby.hansen@outlook.dk>
2023-12-12 13:52:42 +0100

    moved header to top

--- api/crew/crewSignup.php
+++ api/crew/crewSignup.php
@@ -1,5 +1,5 @@
 <?php
-
+header('Content-Type: application/json');
 // header("Access-Control-Allow-Origin: *");
 // header("Access-Control-Allow-Methods: POST, OPTIONS");
 // header("Access-Control-Allow-Headers: Content-Type");
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-header('Content-Type: application/json');
+
 // echo json_encode($response);
 
 ?>
